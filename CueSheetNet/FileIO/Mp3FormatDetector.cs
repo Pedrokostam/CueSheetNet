1a72aea197c6bfd7da33234b8a94defae3daefec
Pedrokostam <74862338+Pedrokostam@users.noreply.github.com>
2023-05-16 20:40:38 +0200

    Added not implemented detector for mp3 and ogg

---
+++ CueSheetNet/FileIO/Mp3FormatDetector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CueSheetNet.FileIO;
+internal class Mp3FormatDetector : IFileFormatReader
+{
+    private readonly string formatName="MP3";
+    private readonly string[] extensions = new string[] {".MP3" };
+
+    public string FormatName => formatName;
+
+    public string[] Extensions => throw new NotImplementedException();
+
+    public bool ExtensionMatches(string fileName) => throw new NotImplementedException();
+    public bool ReadMetadata(Stream stream, out FileMetadata metadata) => throw new NotImplementedException();
+}
---
+++ CueSheetNet/FileIO/OggFormatDetector.cs
@@ -0,0 +1,25 @@
+﻿using CueSheetNET.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CueSheetNet.FileIO;
+internal class OggFormatDetector : IFileFormatReader
+{
+    private readonly string[] extensions = new string[] { ".OGG", ".OGX", ".SPX" };
+    private readonly string formatName = "Ogg";
+    private readonly byte[] OggS = new byte[] { 0x4f, 0x67, 0x67, 0x53 };
+    private readonly byte[] WAVE = new byte[] { 0x57, 0x41, 0x56, 0x45 };
+    public string FormatName => formatName;
+    public string[] Extensions => extensions;
+    public bool ExtensionMatches(string fileName)
+    {
+        string ext = Path.GetExtension(fileName);
+        return extensions.Contains(ext, StringComparer.OrdinalIgnoreCase);
+    }
+
+    public bool ReadMetadata(Stream stream, out FileMetadata metadata) => throw new NotImplementedException();
+}
