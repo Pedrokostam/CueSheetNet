4bc36dc9802791a8d77f685a6cc962817377b9e7
Pedrokostam <74862338+Pedrokostam@users.noreply.github.com>
2024-04-20 00:00:45 +0200

    Documentation is now generated, added nuget packaging

--- livetest.ps1
+++ livetest.ps1
@@ -3,12 +3,13 @@ if(-not $exist){
     Write-Host "Insta"
     Write-Host "C# REPL is not installed. Install it with 'dotnet tool install -g csharprepl'" -ea stop
 }
-$dllPath = join-path $PSScriptRoot "CueSheetNet.Prototyper\bin\Debug\net8.0\CueSheetNet.dll"
+$dllPath = join-path $PSScriptRoot "CueSheetNet\bin\Debug\net8.0\CueSheetNet.dll"
 
-$params=@{
-    r = $dllPath
-    u = 'CueSheetNet'
+# $nugetPath =  ls (join-path $PSScriptRoot CueSheetNet\bin\) -recurse -filter *.nupkg | sort fullname | select -last 1
+if(-not $dllPath){
+    Write-Error "No nuget file!" -ea stop
 }
+Write-host "Adding $dllPath"
 pushd $PSScriptRoot
 csharprepl  $PSScriptRoot/setup.csx -r $dllPath -u CueSheetNet
 popd
